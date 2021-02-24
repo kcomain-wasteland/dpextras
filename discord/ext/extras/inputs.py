38440075aa003555e2067872869d00d8d60d9d76
Kenny Cheung <me@kcomain.dev>
2021-02-25 00:45:44 +0800

    refactor!: move all code to /dpextras so it doesn't break docs

--- discord/ext/extras/inputs.py
+++
@@ -1,6 +0,0 @@
-from discord import Embed
-
-
-def dialog(ctx):
-    """Show a dialog """
-    print('work in progress testing text')
---
+++ dpextras/inputs.py
@@ -0,0 +1,10 @@
+from discord import Embed
+from discord.ext.commands import Context
+
+
+async def dialog(ctx: Context):
+    """Show a dialog
+
+    :param ctx: :ref:`Context <dpy:ext_commands_api_context>`
+    """
+    print('work in progress testing text')
